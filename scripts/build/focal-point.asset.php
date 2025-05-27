769a868a3088d0241edc77a2c0e7348e6e77c7f0
Maxemilian Grönblom <maxemiliang@gmail.com>
2025-05-27 23:00:43 +0300

    feat(focal-point): Focal point works and adds the focal point styles :sparkles:

--- scripts/build/focal-point.asset.php
+++ scripts/build/focal-point.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '3e430cd57c9fff06cf50');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '1704199a191bb88ae5a7');
